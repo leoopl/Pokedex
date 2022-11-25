37e1163c00eaafc65cc4e0daa10294db59c92e28
leoopl <leo.leite@hotmail.com>
2022-11-25 16:44:33 -0300

    data fetch in a new router

--- pages/pokemon/[pokeid].tsx
+++ pages/pokemon/[pokeid].tsx
@@ -1,42 +1,30 @@
-import { useRouter } from 'next/router';
 import { PokemonClient } from 'pokenode-ts';
-import { useEffect, useState } from 'react';
 import style from './pokeid.module.css';
-import Pokemon from '../../utils/ApiType';
 
-export default function poke() {
-	const router = useRouter();
-	const id = router.query;
+export async function getServerSideProps(context) {
+	const id = context.query;
+	console.log(id);
 
-	// console.log(id.pokeid);
-	// const numberID = Number(id.pokeid);
-	// console.log(typeof numberID);
+	const api = new PokemonClient();
+	const data = await api.getPokemonById(+id.pokeid);
 
-	// const [endData, setEndData] = useState<Pokemon>();
-	// useEffect(() => {
-	// 	(async () => {
-	// 		const api = new PokemonClient();
-	// 		await api
-	// 			.getPokemonById(numberID)
-	// 			.then(data => setEndData(data))
-	// 			.catch(error => console.error(error));
-	// 	})();
-	// }, []);
+	return {
+		props: { data },
+	};
+}
 
+export default function poke({ data }) {
 	return (
 		<section className={style.container}>
 			<div className={style.header}>
 				<div className={style.arrow} />
-				<h1 className={style.name}>Bulbasaur</h1>
-				<p className={style.number}>#001</p>
+				<h1 className={style.name}>{data.name}</h1>
+				<p className={style.number}>#0{data.id}</p>
 			</div>
 			<section className={style.card}>
 				<div className={style.type}></div>
 			</section>
-			<img
-				className={style.image}
-				src="https://www.clipartmax.com/png/middle/96-968115_bulbasaur-png.png"
-			/>
+			<img className={style.image} src={data.sprites.front_default} />
 		</section>
 	);
 }
